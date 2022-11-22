d36f7db54e6445242c68e48e74dc839585fd0ebe
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-22 13:53:25 +0100

    added img from objarray

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
-const imgArray=[
-`img/01.webp`,
-`img/02.webp`,
-`img/03.webp`,
-`img/04.webp`,
-`img/05.webp`,
+// array con le img
+const imgArray = [ 
+  { 
+    image: 'img/01.webp',
+    title: 'Marvel\'s Spiderman Miles Morale',
+    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', 
+  },
+  { 
+    image: 'img/02.webp',
+    title: 'Ratchet & Clank: Rift Apart',
+    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, 
+  { 
+    image: 'img/03.webp',
+    title: 'Fortnite',
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos."
+  },
+  {
+    image: 'img/04.webp',
+    title: 'Stray',
+    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  },
+
+  { 
+    image: 'img/05.webp',
+    title: "Marvel's Avengers", 
+    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', 
+  } 
+];
 
 
-]
-// array con img
+
 
 let bigImg=document.getElementById("output");
 let minImg=document.getElementById("colonna");
 let attiva= 0;
 let pointercontainer=document.getElementById("pointer-container");
+
 // Dichiaro i due container delle img e puntatore
 
 for (let i = 0 ; i < imgArray.length ; i++) {
-  bigImg.innerHTML+=`<img src="${imgArray[i]}" alt="" class="none" id="img-${i}">`;
-  minImg.innerHTML+=`<img src="${imgArray[i]}" alt="" class="" id="min-${i}">`
-  pointercontainer.innerHTML+=`<input type="radio" name="indicator" data-order="1" id="ind-${i}">`
+  let objArr = imgArray[i];
+  
+  bigImg.innerHTML+=`<img src="${objArr.image}" alt="" class="none" id="img-${i}">`;
+  minImg.innerHTML+=`<img src="${objArr.image}" alt="" class="" id="min-${i}">`;
+  pointercontainer.innerHTML+=`<input type="radio" name="indicator" data-order="1" id="ind-${i}">`;
+  onImgTxt.innerHTML += `<span class="testo1">asdsssssssssssssssss</span>`;
+  
+  
   
 }
 // Inseriamo le img grandi e piccola
@@ -139,3 +167,4 @@ indicatorQuattro.addEventListener("click", function(){
     bigActive.classList.add("animate__animated")
     bigActive.classList.add("animate__fadeIn")
 }); 
+
